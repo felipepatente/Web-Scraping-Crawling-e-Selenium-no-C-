3ec292f8bf9c264c38e18dc5a5e43056671cc10b
felipepatente <felipepatente@yahoo.com.br>
2020-07-01 13:24:23 -0300

    Mostrando os valores que foram pegos

--- Bot/Bot.Instagram.Profile/Program.cs
+++ Bot/Bot.Instagram.Profile/Program.cs
@@ -7,6 +7,19 @@ namespace Bot.Instagram.Profile
         static void Main(string[] args)
         {
             Profile profile = Instagram.GetProfileByUser("i.love.code");
+
+            Console.WriteLine($"UserName = {profile.UserName}");
+            Console.WriteLine($"Title = {profile.Title}");
+            Console.WriteLine($"Description = {profile.Description}");
+            Console.WriteLine($"Url = {profile.Url}");
+            Console.WriteLine($"Image = {profile.Image}");
+            Console.WriteLine($"AndroidAppId = {profile.AndroidAppId}");
+            Console.WriteLine($"AndroidAppName = {profile.AndroidAppName}");
+            Console.WriteLine($"AndroidUrl = {profile.AndroidUrl}");
+            Console.WriteLine($"IosAppId = {profile.IosAppId}");
+            Console.WriteLine($"IosAppName = {profile.IosAppName}");
+            Console.WriteLine($"IosUrl = {profile.IosUrl}");
+
         }
     }
 }
