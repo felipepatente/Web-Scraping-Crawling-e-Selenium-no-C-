3a200e9d732c0237545420c44e32c63eb4729a68
felipepatente <felipepatente@yahoo.com.br>
2020-07-01 13:21:33 -0300

    Robô para obter perfil do Instagram

---
+++ Bot/Bot.Instagram.Profile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bot.Instagram.Profile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Profile profile = Instagram.GetProfileByUser("i.love.code");
+        }
+    }
+}
