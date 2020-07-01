6ef1242ccff911d77f255342523d456267d18871
felipepatente <felipepatente@yahoo.com.br>
2020-07-01 10:26:47 -0300

    Robô para obter resultado da Mega Sena utiliznado JSON

---
+++ Bot/Bot.MegaSena.JSON/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+
+namespace Bot.MegaSena.JSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            string url = @"http://loterias.caixa.gov.br/wps/portal/loterias/landing/megasena/!ut/p/a1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOLNDH0MPAzcDbwMPI0sDBxNXAOMwrzCjA0sjIEKIoEKnN0dPUzMfQwMDEwsjAw8XZw8XMwtfQ0MPM2I02-AAzgaENIfrh-FqsQ9wNnUwNHfxcnSwBgIDUyhCvA5EawAjxsKckMjDDI9FQE-F4ca/dl5/d5/L2dBISEvZ0FBIS9nQSEh/pw/Z7_HGK818G0KO6H80AU71KG7J0072/res/id=buscaResultado/c=cacheLevelPage/=/?timestampAjax=1593608679095";
+            string json;
+
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.Headers["Cookie"] = "security=true";
+                json = webClient.DownloadString(url);
+            }
+
+            var resultadoDaMegaSena = JsonConvert.DeserializeObject<Resultado>(json);
+
+            Console.WriteLine(resultadoDaMegaSena.resultadoOrdenado);
+        }
+    }
+}
